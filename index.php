3abed8563e9da6570cb5681014b3e8ebde4a6a44
Matthew.ekpoto <idomatthew@gmail.com>
2018-04-14 01:55:28 +0100

    email-project

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>email template</title>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+</head>
+
+<body>
+      <br />
+      <div class="container">
+      	<div class="row">
+      		<div class="col-md-12">
+            	<div class="banner text-center">
+              		
+              			<p class="title"> hotels.ng</p>
+                      <p class="sub-title"> The Biggest Reservation Agency in Nigeria </p> <br/>
+            	            		  
+            	</div>
+      		</div>
+      	</div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="a text-center">
+              <p class="ab">Request for Reservation at your hotel</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <br />
+    <div class="wrapper">
+      <!-- top -->
+
+      <br />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p class="b"> <b> Hello at southern Sun ikoyi Hotel,</b></p>
+            <p>as discussed on the phone, kindly make reservation for our client. the details are stated below.</p>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <div class="container">
+        <div class="row">
+
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title text-center">client Name</h5>
+                <p class="card-text">Mr Abidakun on behalf <br> of Mr.Abayomi abidakun</p>
+              </div>
+            </div>
+          </div>
+          
+
+          <div class="col-sm-5">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Schedule Details</h5>
+                <p class="card-text">Arrival Date:  17th December  <br /> Departure Date: 18th December.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <table class="table">
+              <tr>
+                <th class="col-md-4">Room Type</th>
+                <th class="col-md-2">No of Rooms</th>
+                <th class="col-md-2">Rate per Room</th>
+                <th class="col-md-2">No of Nights</th>
+                <th class="col-md-2">sub-total</th>
+              </tr>
+              <tr>
+                <td>standard promo (Dec 12 -Jan 9: free buffet, breakfast for 2, complimetry shirt ironed)</td>
+                <td>1</td>
+                <td>N 0</td>
+                <td>1</td>
+                <td>N 0</td>
+              </tr>
+            </table>
+          </div>
+        </div> 
+      </div>
+
+      <br />
+      <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="tag text-right"><b>Grand Total(Rack Rate):N 35,000</b></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <br />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <p>Kindly Comfirm this Reservation as soon as possible and send me feedback. if you require any futher information, do nit hesitate to call me on thid phone number <strong> 07088088800 or reply this email</strong>
+            </p>
+          </div>
+        </div>
+      </div> <br />
+      <hr>
+
+
+      <div class="container">
+        <div class="row">
+
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <p class="nick text-left">Best Regards <br><b>Opeyemi Jegede</b><br> Bookings Team</p>
+              </div>
+            </div>
+          </div>
+          
+
+          <div class="col-sm-5">
+            <div class="card">
+              <div class="card-body">
+                <p class="mat text-right">Email:opeyemi@hotels.ng</p>
+                <p class="mat text-right">Phone:07088088800 <br /></p>
+              </div>
+            </div>
+          </div> 
+
+          <p align="text-center">this email was sent to ifekt4god@gmail.com for hotel reservation purposes.</p>
+          <p>where you satisfied with our service? if no, e-mail your complain to our managing Director md@hotels.ng</p>
+          <hr>
+        </div>
+      </div>
+
+    </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      
+      <footer> <p> Hotels.ng is an online booking agency specialised in hotels booking withing Nigera. we market hotels online and book rooms for clients, we proffer hotel accommodation solution to people looking for hotels withing an arear or paticular city.<br>
+       we provide them with appropriate hotels that match their budget and expectation </p></footer>
+
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
